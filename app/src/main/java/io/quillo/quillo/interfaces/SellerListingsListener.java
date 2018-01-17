6b49a52f095473e4153a58276fb46bdb3e79a14a
Tamir Shklaz <shklazt@gmail.com>
2018-01-17 18:06:16 +0200

    Installed Firebase Added Constants class

--- app/src/main/java/io/quillo/quillo/interfaces/SellerListingsListener.java
+++ app/src/main/java/io/quillo/quillo/interfaces/SellerListingsListener.java
@@ -2,15 +2,13 @@ package io.quillo.quillo.interfaces;
 
 import android.view.View;
 
-import java.util.List;
-
 import io.quillo.quillo.data.Listing;
 
 /**
  * Created by Stickells on 15/01/2018.
  */
 
-public interface sellerListingsListener {
+public interface SellerListingsListener {
 
     void startListingDetailActivity(Listing listing, View viewRoot);
 
