8076a30e90f9615ca8223fc3c9ebfb354bdd62f6
Tamir <shklazt@gmail.com>
2018-01-22 13:27:10 +0200

    Refactored SearchActivity into search fragment

--- app/src/main/java/io/quillo/quillo/Fragments/SearchFragment.java
+++ app/src/main/java/io/quillo/quillo/Fragments/SearchFragment.java
@@ -4,31 +4,109 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
 import io.quillo.quillo.R;
+import io.quillo.quillo.controllers.ListingAdapter;
+import io.quillo.quillo.data.CustomFirebaseDatabase;
+import io.quillo.quillo.data.Listing;
+import io.quillo.quillo.interfaces.ListingCellListener;
+import io.quillo.quillo.interfaces.ListingsListener;
 
 /**
  * Created by shkla on 2018/01/22.
+ *
+ *
  */
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements ListingsListener, ListingCellListener {
+
 
     public static SearchFragment newInstance(){
         SearchFragment searchFragment = new SearchFragment();
         return  searchFragment;
     }
 
+    private RecyclerView recyclerView;
+    private ListingAdapter adapter;
+    private android.support.v7.widget.Toolbar toolbar;
+
+    private CustomFirebaseDatabase customFirebaseDatabase;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(getActivity());
+        adapter = new ListingAdapter(this, getContext());
+        customFirebaseDatabase = new CustomFirebaseDatabase();
+        customFirebaseDatabase.setListingsListener(this);
+        customFirebaseDatabase.queryListings("");
+
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_home_search, container, false);
+        View view = inflater.inflate(R.layout.activity_home_search, container, false);
+        setUpView(view);
+        return view;
+
+
     }
+
+    //TODO The majority of this code and functionality is duplicated in ProfileActivity, fix up.
+
+    public void startListingDetailActivity(Listing listing) {
+        //TODO Update nav to use fragments
+        /*Intent intent = new Intent(this, ListingDetailActivity.class);
+        intent.putExtra(IntentExtras.EXTRA_LISTING, listing);
+
+        startActivity(intent);*/
+    }
+
+    public void setUpView(View view) {
+        recyclerView = (RecyclerView) view.findViewById(R.id.rec_home_search_listing_holder);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
+        itemDecoration.setDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.divider_white));
+        recyclerView.addItemDecoration(itemDecoration);
+    }
+
+    @Override
+    public void onListingLoaded(Listing newListing) {
+        adapter.addListing(newListing);
+    }
+
+    @Override
+    public void onListingUpdated(Listing listing) {
+        adapter.updateListing(listing);
+    }
+
+    @Override
+    public void onBookmarkClick(Listing listing) {
+        customFirebaseDatabase.addBookmark(listing);
+    }
+
+    @Override
+    public void onUnBookmarkClick(Listing listing) {
+        customFirebaseDatabase.removeBookmark(listing);
+    }
+
+    @Override
+    public void onListingClick(Listing listing) {
+        startListingDetailActivity(listing);
+    }
+
 }
