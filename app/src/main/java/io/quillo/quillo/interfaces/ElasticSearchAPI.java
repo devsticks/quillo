4c3eb722072df8ef357b485ebfe9344f0d2819a5
Tom Tumiel <13222731+Tom2718@users.noreply.github.com>
2018-01-31 14:47:24 +0200

    elasticsearch querying api

---
+++ app/src/main/java/io/quillo/quillo/interfaces/ElasticSearchAPI.java
@@ -0,0 +1,25 @@
+package io.quillo.quillo.interfaces;
+
+import java.util.Map;
+import io.quillo.quillo.utils.HitsObject;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Query;
+
+/**
+ * Created by Tom on 2018/01/28.
+ */
+
+public interface ElasticSearchAPI {
+
+    @GET("_search/")
+    Call<HitsObject> search(
+            @HeaderMap Map<String, String> headers,
+            @Query("default_operator") String operator,
+            @Query("q") String query,
+            @Query("size") int size,
+            @Query("from") int from
+
+    );
+}
